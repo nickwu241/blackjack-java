74ca341fd5ca8625433177037f502eeef0b08c21
Nicholas Wu <nickwu241@gmail.com>
2016-09-04 16:06:03 -0700

    Improve game messages

--- src/fundementals/blackjacktable/BlackjackTable.java
+++ src/fundementals/blackjacktable/BlackjackTable.java
@@ -172,7 +172,7 @@ public class BlackjackTable
       }
       if (hand.busted())
       {
-         UTIL.gameMsg(hand.getOwner().getName() + ": BUSTED");
+         UTIL.gameMsg(hand.getOwner().getName() + ": BUSTED with " + hand.asString() + "(" + hand.getValue() + ")");
       }
       return handComplete || hand.busted();
    }
